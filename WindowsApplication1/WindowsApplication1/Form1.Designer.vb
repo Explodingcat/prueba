31c40df926b142038f25969468a3752c2016f2fb
Felipe Roa <roa.aguilera.felipe@gmail.com>
2018-09-25 09:43:52 -0300

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.TextBox5 = New System.Windows.Forms.TextBox
+        Me.TextBox6 = New System.Windows.Forms.TextBox
+        Me.TextBox7 = New System.Windows.Forms.TextBox
+        Me.Camb_c = New System.Windows.Forms.Button
+        Me.camb_tb = New System.Windows.Forms.TextBox
+        Me.Limpiar = New System.Windows.Forms.Button
+        Me.Users = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(83, 149)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(71, 87)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Creasys.25"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(71, 213)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.Text = "creasys.local"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(338, 45)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(392, 20)
+        Me.TextBox4.TabIndex = 5
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(335, 82)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(394, 20)
+        Me.TextBox5.TabIndex = 6
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(338, 122)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(390, 20)
+        Me.TextBox6.TabIndex = 7
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(335, 167)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(394, 20)
+        Me.TextBox7.TabIndex = 8
+        '
+        'Camb_c
+        '
+        Me.Camb_c.Location = New System.Drawing.Point(773, 213)
+        Me.Camb_c.Name = "Camb_c"
+        Me.Camb_c.Size = New System.Drawing.Size(75, 23)
+        Me.Camb_c.TabIndex = 9
+        Me.Camb_c.Text = "Cambio Contraseña"
+        Me.Camb_c.UseVisualStyleBackColor = True
+        '
+        'camb_tb
+        '
+        Me.camb_tb.Location = New System.Drawing.Point(759, 177)
+        Me.camb_tb.Name = "camb_tb"
+        Me.camb_tb.Size = New System.Drawing.Size(100, 20)
+        Me.camb_tb.TabIndex = 10
+        '
+        'Limpiar
+        '
+        Me.Limpiar.Location = New System.Drawing.Point(773, 38)
+        Me.Limpiar.Name = "Limpiar"
+        Me.Limpiar.Size = New System.Drawing.Size(75, 23)
+        Me.Limpiar.TabIndex = 11
+        Me.Limpiar.Text = "Limpiar"
+        Me.Limpiar.UseVisualStyleBackColor = True
+        '
+        'Users
+        '
+        Me.Users.AutoSize = True
+        Me.Users.Location = New System.Drawing.Point(51, 13)
+        Me.Users.Name = "Users"
+        Me.Users.Size = New System.Drawing.Size(0, 13)
+        Me.Users.TabIndex = 12
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(210, 122)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(885, 262)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Users)
+        Me.Controls.Add(Me.Limpiar)
+        Me.Controls.Add(Me.camb_tb)
+        Me.Controls.Add(Me.Camb_c)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Prueba AD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents Camb_c As System.Windows.Forms.Button
+    Friend WithEvents camb_tb As System.Windows.Forms.TextBox
+    Friend WithEvents Limpiar As System.Windows.Forms.Button
+    Friend WithEvents Users As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,91 @@
+﻿Imports System.DirectoryServices
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Users.Text = Environ("username")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Usr As String = Users.Text
+        Dim Pas As String = TextBox2.Text
+        Dim Dom As String = TextBox3.Text
+        Dim Success As Boolean = False
+        Dim Entry As New System.DirectoryServices.DirectoryEntry("LDAP://" & Dom)
+        Dim Searcher As New System.DirectoryServices.DirectorySearcher(Entry)
+        Searcher.SearchScope = DirectoryServices.SearchScope.OneLevel
+        Try
+            Dim Results As System.DirectoryServices.SearchResult = Searcher.FindOne
+            Success = Not (Results Is Nothing)
+        Catch
+            Success = False
+        End Try
+        If Success = True Then
+            Dim currentADUser As System.DirectoryServices.AccountManagement.UserPrincipal
+            currentADUser = System.DirectoryServices.AccountManagement.UserPrincipal.Current
+            Dim userEmail As String = currentADUser.EmailAddress
+
+            Dim userFullName As String = currentADUser.Name
+            TextBox4.Text = userFullName
+            TextBox5.Text = userEmail
+            TextBox6.Text = currentADUser.Description.ToString()
+            TextBox7.Text = currentADUser.DistinguishedName.ToString()
+        Else
+            MsgBox("No existen")
+        End If
+        Entry.Close()
+    End Sub
+
+    Private Sub Camb_c_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Camb_c.Click
+        Dim Usr As String = Users.Text
+        Dim Pas As String = TextBox2.Text
+        Dim Dom As String = TextBox3.Text
+        Dim Success As Boolean = False
+        Dim Entry As New System.DirectoryServices.DirectoryEntry("LDAP://" & Dom, Usr, Pas)
+        Dim Searcher As New System.DirectoryServices.DirectorySearcher(Entry)
+        Dim nueva As String
+        Searcher.SearchScope = DirectoryServices.SearchScope.OneLevel
+        Try
+            Dim Results As System.DirectoryServices.SearchResult = Searcher.FindOne
+            Success = Not (Results Is Nothing)
+        Catch
+            Success = False
+        End Try
+        If Success = True Then
+            Try
+                Dim currentADUser As System.DirectoryServices.AccountManagement.UserPrincipal
+                currentADUser = System.DirectoryServices.AccountManagement.UserPrincipal.Current
+                nueva = camb_tb.Text
+                currentADUser.ChangePassword(Pas, nueva)
+                MsgBox("Contraseña Cambiada")
+            Catch
+                Dim currentADUser As System.DirectoryServices.AccountManagement.UserPrincipal
+                currentADUser = System.DirectoryServices.AccountManagement.UserPrincipal.Current
+                MsgBox(currentADUser.LastPasswordSet)
+            End Try
+        Else
+            MsgBox("No existen")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Limpiar.Click
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+    End Sub
+
+    Private Sub Button2_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim Usr As String = Users.Text
+        Dim Pas As String = TextBox2.Text
+        Dim Dom As String = TextBox3.Text
+        Dim authentic As Boolean = False
+        Try
+            Dim entry As New DirectoryEntry("LDAP://" & Dom, Usr, Pas)
+            Dim nativeObject As Object = entry.NativeObject
+            authentic = True
+        Catch generatedExceptionName As DirectoryServicesCOMException
+        End Try
+        MsgBox(authentic)
+    End Sub
+End Class
